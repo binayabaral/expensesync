84184feb41b361c0407f965fd9c87a45c210fa52
Binaya Baral <binaya.baral5@gmail.com>
2024-12-14 17:52:16 +0545

    feat: setup authentication

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,23 @@
+import { Loader2 } from 'lucide-react';
+import { SignUp, ClerkLoaded, ClerkLoading } from '@clerk/nextjs';
+
+export default function Page() {
+  return (
+    <div className='min-h-screen'>
+      <div className='h-full flex-col items-center justify-center'>
+        <div className='text-center space-y-4 pt-[10vh]'>
+          <h1 className='font-bold text-3xl'>Welcome back!</h1>
+          <p className='text-base text-muted-foreground'>Log in or create account to get back to your dashboard</p>
+        </div>
+        <div className='flex items-center justify-center mt-8'>
+          <ClerkLoaded>
+            <SignUp path='/sign-up' />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className='animate-spin text-muted-foreground' />
+          </ClerkLoading>
+        </div>
+      </div>
+    </div>
+  );
+}
