52dd2ba5e16a2cf52dca2f53ce78d7750aba7aee
Binaya Baral <binaya.baral5@gmail.com>
2024-12-16 00:29:30 +0545

    feat: add loader for header welcome

--- components/HeaderWelcome.tsx
+++ components/HeaderWelcome.tsx
@@ -3,15 +3,24 @@
 import React from 'react';
 import { useUser } from '@clerk/nextjs';
 
+import { Skeleton } from '@/components/ui/skeleton';
+
 function HeaderWelcome() {
   const { isLoaded, user } = useUser();
 
   return (
     <div className='pt-32'>
-      <h1 className='text-2xl lg:text-5xl text-white font-medium mb-3'>
-        Welcome Back, {isLoaded ? `${user?.firstName} 👋` : ''}
-      </h1>
-      <p className='text-green-300'>This is your financial overview report</p>
+      {isLoaded && user?.firstName ? (
+        <>
+          <h1 className='text-2xl lg:text-5xl text-white font-medium mb-3'>Welcome Back, {user?.firstName} 👋</h1>
+          <p className='text-green-300'>This is your financial overview report</p>
+        </>
+      ) : (
+        <>
+          <Skeleton className='h-8 lg:h-12 w-full md:w-1/2 bg-green-500 mb-3' />
+          <Skeleton className='h-6 w-full md:w-1/3 bg-green-500' />
+        </>
+      )}
     </div>
   );
 }
