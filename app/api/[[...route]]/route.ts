80faf10ab7f50fe57cb85d856799ff5c38da1bf4
Binaya Baral <binaya.baral5@gmail.com>
2024-12-20 23:25:38 +0545

    feat: remove usage of HTTPException

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,6 +1,5 @@
 import { Hono } from 'hono';
 import { handle } from 'hono/vercel';
-import { HTTPException } from 'hono/http-exception';
 
 import accountsRoute from './accounts';
 
@@ -10,14 +9,6 @@ const app = new Hono().basePath('/api');
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const routes = app.route('/accounts', accountsRoute);
-
-app.onError((err, c) => {
-  if (err instanceof HTTPException) {
-    return err.getResponse();
-  }
-
-  return c.json({ error: 'Internal Server Error' }, 500);
-});
 
 export const GET = handle(app);
 export const POST = handle(app);
