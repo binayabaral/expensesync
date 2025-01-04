b5d6bf9ec9f9da01ca7c877384cb6990f6d6a2de
Binaya Baral <binaya.baral5@gmail.com>
2025-01-04 12:38:06 +0545

    fix: logic to fill missing days

--- lib/utils.ts
+++ lib/utils.ts
@@ -55,10 +55,17 @@ export function fillMissingDays(
   const allDays = eachDayOfInterval({ start: startDate, end: endDate });
 
   const transactionsByDay = allDays.map(day => {
-    const found = activeDays.find(d => isSameDay(d.date, day));
+    const found = activeDays.filter(d => isSameDay(d.date, day));
 
-    if (found) {
-      return found;
+    if (found.length > 0) {
+      return found.reduce(
+        (acc, curr) => ({
+          date: day,
+          income: acc.income + curr.income,
+          expenses: acc.expenses + curr.expenses
+        }),
+        { date: day, income: 0, expenses: 0 }
+      );
     } else {
       return {
         date: day,
