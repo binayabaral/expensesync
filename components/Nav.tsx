1ea689b2de3b4f02207f2425d9acf896c8504da9
Binaya Baral <binaya.baral5@gmail.com>
2025-10-31 15:09:48 +0545

    feat: create a tablet friendly navigation

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import isMobile from 'is-mobile';
+import { useMedia } from 'react-use';
 import { Loader2 } from 'lucide-react';
 import { useState, Fragment } from 'react';
 import { usePathname, useRouter } from 'next/navigation';
@@ -45,7 +46,10 @@ function Nav() {
   const router = useRouter();
   const { user } = useUser();
   const pathName = usePathname();
+  const isSmallerScreen = useMedia('(max-width: 1024px)', false);
   const isMobileDevice = isMobile();
+
+  const enableMobileNav = isSmallerScreen || isMobileDevice;
 
   const onSheetButtonClick = (href: string) => {
     router.push(href);
@@ -59,23 +63,23 @@ function Nav() {
           <UserButton
             appearance={{
               elements: {
-                userButtonAvatarBox: cn(isMobileDevice ? 'size-5' : 'size-7')
+                userButtonAvatarBox: cn(enableMobileNav ? 'size-5' : 'size-7')
               }
             }}
           />
-          <span className={cn('mt-1 text-[11px]', isMobileDevice ? '' : 'hidden')}>{user?.fullName}</span>
+          <span className={cn('mt-1 text-[11px]', enableMobileNav ? '' : 'hidden')}>{user?.fullName}</span>
         </div>
       </ClerkLoaded>
       <ClerkLoading>
-        <Loader2 className={cn('animate-spin', isMobileDevice ? 'size-5' : 'size-7')} />
+        <Loader2 className={cn('animate-spin', enableMobileNav ? 'size-5' : 'size-7')} />
       </ClerkLoading>
     </>
   );
 
   return (
     <>
-      <nav className={cn('flex items-center', isMobileDevice ? 'w-full justify-between' : 'gap-x-2')}>
-        {routes.slice(0, isMobileDevice ? 3 : routes.length).map((route, index) => (
+      <nav className={cn('flex items-center', enableMobileNav ? 'w-full justify-between' : 'gap-x-2')}>
+        {routes.slice(0, enableMobileNav ? 3 : routes.length).map((route, index) => (
           <Fragment key={route.label}>
             <Button
               onClick={() => router.push(route.href)}
@@ -83,13 +87,13 @@ function Nav() {
               className={cn(
                 'justify-between font-normal border-none outline-none transition shadow-none hover:bg-green-500/10 hover:text-primary',
                 pathName === route.href ? 'bg-green-500/10 text-primary' : 'bg-transparent',
-                isMobileDevice ? 'w-1/5 py-5 text-[11px] px-1 h-auto flex flex-col border border-r' : 'mr-1'
+                enableMobileNav ? 'w-1/5 py-5 text-[11px] px-1 h-auto flex flex-col border border-r' : 'mr-1'
               )}
             >
-              <route.icon className={cn(isMobileDevice ? 'size-7' : 'hidden')} />
+              <route.icon className={cn(enableMobileNav ? 'size-7' : 'hidden')} />
               <span className='block'>{route.label}</span>
             </Button>
-            {index === 1 && isMobileDevice && (
+            {index === 1 && enableMobileNav && (
               <Sheet open={isSheetOpen} onOpenChange={setIsSheetOpen}>
                 <SheetTrigger asChild>
                   <Button
@@ -97,10 +101,10 @@ function Nav() {
                     className={cn(
                       'justify-between font-normal border-none outline-none transition shadow-none hover:bg-green-500/10 hover:text-primary',
                       isSheetOpen ? 'bg-green-500/10 text-primary' : 'bg-transparent',
-                      isMobileDevice ? 'w-1/5 py-5 text-[11px] px-1 h-auto flex flex-col border border-r' : 'mr-1'
+                      enableMobileNav ? 'w-1/5 py-5 text-[11px] px-1 h-auto flex flex-col border border-r' : 'mr-1'
                     )}
                   >
-                    <FaCirclePlus className={cn(isMobileDevice ? 'size-7' : 'hidden')} />
+                    <FaCirclePlus className={cn(enableMobileNav ? 'size-7' : 'hidden')} />
                     All Options
                   </Button>
                 </SheetTrigger>
@@ -116,10 +120,10 @@ function Nav() {
                           className={cn(
                             'justify-between font-normal border-none outline-none transition shadow-none hover:bg-green-500/10 hover:text-primary',
                             pathName === route.href ? 'bg-green-500/10 text-primary' : 'bg-transparent',
-                            isMobileDevice ? 'w-1/4 py-5 text-[11px] px-1 h-20 flex flex-col border border-r' : 'mr-1'
+                            enableMobileNav ? 'w-1/4 py-5 text-[11px] px-1 h-20 flex flex-col border border-r' : 'mr-1'
                           )}
                         >
-                          <route.icon className={cn(isMobileDevice ? 'size-7' : 'hidden')} />
+                          <route.icon className={cn(enableMobileNav ? 'size-7' : 'hidden')} />
                           <span className='block'>{route.label}</span>
                         </Button>
                       ))}
@@ -135,7 +139,7 @@ function Nav() {
             )}
           </Fragment>
         ))}
-        <div className={cn(isMobileDevice ? 'w-1/5 text-center' : '')}>{renderUserAvatar()}</div>
+        <div className={cn(enableMobileNav ? 'w-1/5 text-center' : '')}>{renderUserAvatar()}</div>
       </nav>
     </>
   );
